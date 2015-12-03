4818087e4c22eb45ff5316af291b61e250b656b2
d3moid <d3moid@users.noreply.github.com>
2015-12-03 16:23:32 +0200

    Initial Commit.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,30 +31,59 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+         it('should have a url defined for each feed', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);
+            });
+         });
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+         it('should have a name defined and not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name.length).not.toBe(0);
+            });
+         });
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('The menu', function() {
+        var body;
+        beforeEach(function() {
+            body = $('body');
+        });
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+         it('should be hidden by default', function() {
+            expect(body.hasClass('menu-hidden')).toBe(true);
+         });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+          it('should trigger visibility when clicked', function() {
+            var menuIcon = $('.menu-icon-link');
+            // Trigger click to display menu
+            menuIcon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(false);
+            // Trigger click to hide menu
+            menuIcon.trigger('click');
+            expect(body.hasClass('menu-hidden')).toBe(true);
+          });
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -62,11 +91,42 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         beforeEach(function(done) {
+            // init loadFeed with callback
+            loadFeed(0, function() {
+                done();
+            });
+         });
 
-    /* TODO: Write a new test suite named "New Feed Selection"
+         it('should have at least a single feed entry element after load', function(done) {
+            expect($('.feed .entry').length).toBe(true);
+            done();
+         });
 
+    });
+
+    /* TODO: Write a new test suite named "New Feed Selection" */
+
+    describe('New Feed Selection', function() {
+        var initialFeed;
+
+        beforeEach(function(done) {
+            // Store the initial feed html content to compare later
+            loadFeed(0, function() {
+                initialFeed = $('.feed').html();
+            });
+        });
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+         it('should change content when new feed loads', function(done) {
+            // Call the next feed and compare html content with initial feed after done
+            loadFeed(1, function() {
+                var newFeed = $('.feed').html();
+                expect(initialFeed).not.toEqual(newFeed);
+                done();
+            });        
+         });
+    });
 }());
